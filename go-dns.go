7bae59cd32100f18742b7df72a6ddf85c1fe3101
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-06-29 17:19:48 -0500

    Add cache.

--- go-dns.go
+++ go-dns.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"math/rand"
 	"net"
 	"os"
 	"os/signal"
+	"strings"
+	"sync/atomic"
 	"syscall"
+	"time"
 
 	"github.com/kr/pretty"
 	"github.com/miekg/dns"
+	"github.com/patrickmn/go-cache"
 )
 
 var logger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lmicroseconds)
@@ -42,22 +47,146 @@ type configuration struct {
 	ForwardNamesToAddresses []forwardNameToAddress `json:"forwardNamesToAddresses"`
 	ReverseDomain           string                 `json:"reverseDomain"`
 	ReverseAddressesToNames []reverseAddressToName `json:"reverseAddressesToNames"`
+	MinTTLSeconds           uint32                 `json:"minTTLSeconds"`
+	MaxTTLSeconds           uint32                 `json:"maxTTLSeconds"`
+	TimerIntervalSeconds    uint32                 `json:"timerIntervalSeconds"`
+}
+
+func pickRandomStringSliceEntry(s []string) string {
+	return s[rand.Intn(len(s))]
+}
+
+func getQuestionCacheKey(m *dns.Msg) string {
+	key := ""
+	first := true
+
+	for _, question := range m.Question {
+		if !first {
+			key += "|"
+		}
+		key += fmt.Sprintf("%s:%d:%d", strings.ToLower(question.Name), question.Qtype, question.Qclass)
+		first = false
+	}
+
+	return key
+}
+
+type metrics struct {
+	cacheHits   uint64
+	cacheMisses uint64
+}
+
+func (metrics *metrics) IncrementCacheHits() {
+	atomic.AddUint64(&metrics.cacheHits, 1)
+}
+
+func (metrics *metrics) CacheHits() uint64 {
+	return atomic.LoadUint64(&metrics.cacheHits)
+}
+
+func (metrics *metrics) IncrementCacheMisses() {
+	atomic.AddUint64(&metrics.cacheMisses, 1)
+}
+
+func (metrics *metrics) CacheMisses() uint64 {
+	return atomic.LoadUint64(&metrics.cacheMisses)
+}
+
+type cacheObject struct {
+	cacheTime      time.Time
+	expirationTime time.Time
+	message        *dns.Msg
+}
+
+func (co *cacheObject) Copy() *cacheObject {
+	return &cacheObject{
+		cacheTime:      co.cacheTime,
+		expirationTime: co.expirationTime,
+		message:        co.message.Copy(),
+	}
 }
 
 type DNSProxy struct {
 	configuration *configuration
 	dnsClient     *dns.Client
+	cache         *cache.Cache
+	metrics       metrics
 }
 
 func NewDNSProxy(configuration *configuration) *DNSProxy {
 	return &DNSProxy{
 		configuration: configuration,
 		dnsClient:     new(dns.Client),
+		cache:         cache.New(cache.NoExpiration, 1*time.Minute),
 	}
 }
 
-func pickRandomStringSliceEntry(s []string) string {
-	return s[rand.Intn(len(s))]
+func (dnsProxy *DNSProxy) getMinTTLSeconds(m *dns.Msg) uint32 {
+	foundTTL := false
+	var minTTLSeconds uint32
+
+	processRR := func(rr dns.RR) {
+		ttl := rr.Header().Ttl
+		if ttl < dnsProxy.configuration.MinTTLSeconds {
+			ttl = dnsProxy.configuration.MinTTLSeconds
+		}
+		if ttl > dnsProxy.configuration.MaxTTLSeconds {
+			ttl = dnsProxy.configuration.MaxTTLSeconds
+		}
+		if (!foundTTL) || (ttl < minTTLSeconds) {
+			minTTLSeconds = ttl
+			foundTTL = true
+		}
+		rr.Header().Ttl = ttl
+	}
+
+	for _, rr := range m.Answer {
+		processRR(rr)
+	}
+	for _, rr := range m.Ns {
+		processRR(rr)
+	}
+
+	return minTTLSeconds
+}
+
+func (dnsProxy *DNSProxy) adjustTTL(cacheObject *cacheObject) bool {
+	valid := true
+	now := time.Now()
+
+	if now.Equal(cacheObject.expirationTime) || now.After(cacheObject.expirationTime) {
+		valid = false
+	}
+
+	var secondsToSubtractFromTTL float64
+	if valid {
+		secondsToSubtractFromTTL = now.Sub(cacheObject.cacheTime).Seconds()
+		if secondsToSubtractFromTTL <= 0 {
+			valid = false
+		}
+	}
+
+	if valid {
+		m := cacheObject.message
+		adjustRRTTL := func(rr dns.RR) {
+			ttl := rr.Header().Ttl
+			ttl -= uint32(secondsToSubtractFromTTL)
+			if ttl <= 0 {
+				valid = false
+			} else {
+				rr.Header().Ttl = ttl
+			}
+		}
+
+		for _, rr := range m.Answer {
+			adjustRRTTL(rr)
+		}
+		for _, rr := range m.Ns {
+			adjustRRTTL(rr)
+		}
+	}
+
+	return valid
 }
 
 func (dnsProxy *DNSProxy) createProxyHandlerFunc() dns.HandlerFunc {
@@ -67,17 +196,55 @@ func (dnsProxy *DNSProxy) createProxyHandlerFunc() dns.HandlerFunc {
 	}
 
 	return func(w dns.ResponseWriter, r *dns.Msg) {
-		originalID := r.Id
-		r.Id = dns.Id()
-		remoteHostAndPort := pickRandomStringSliceEntry(remoteHostAndPortStrings)
-		resp, _, err := dnsProxy.dnsClient.Exchange(r, remoteHostAndPort)
-		if err != nil {
-			logger.Printf("dnsClient exchange remoteHostAndPort = %v error = %v", remoteHostAndPort, err.Error())
-			r.Id = originalID
-			dns.HandleFailed(w, r)
-		} else {
-			resp.Id = originalID
-			w.WriteMsg(resp)
+
+		requestID := r.Id
+		responded := false
+
+		requestQuestionCacheKey := getQuestionCacheKey(r)
+
+		co, ok := dnsProxy.cache.Get(requestQuestionCacheKey)
+		if ok {
+			cacheObjectCopy := co.(*cacheObject).Copy()
+			if dnsProxy.adjustTTL(cacheObjectCopy) {
+				dnsProxy.metrics.IncrementCacheHits()
+
+				msg := cacheObjectCopy.message
+				msg.Id = requestID
+				w.WriteMsg(msg)
+				responded = true
+			}
+		}
+
+		if !responded {
+			dnsProxy.metrics.IncrementCacheMisses()
+			r.Id = dns.Id()
+			remoteHostAndPort := pickRandomStringSliceEntry(remoteHostAndPortStrings)
+			resp, _, err := dnsProxy.dnsClient.Exchange(r, remoteHostAndPort)
+			if err != nil {
+				r.Id = requestID
+				dns.HandleFailed(w, r)
+			} else {
+
+				if (resp.Rcode == dns.RcodeSuccess) || (resp.Rcode == dns.RcodeNameError) {
+					minTTLSeconds := dnsProxy.getMinTTLSeconds(resp)
+					respQuestionCacheKey := getQuestionCacheKey(resp)
+
+					if (len(respQuestionCacheKey) > 0) && (minTTLSeconds > 0) {
+						ttlDuration := time.Second * time.Duration(minTTLSeconds)
+						now := time.Now()
+						expirationTime := now.Add(ttlDuration)
+						cacheObject := &cacheObject{
+							cacheTime:      now,
+							expirationTime: expirationTime,
+							message:        resp.Copy(),
+						}
+						dnsProxy.cache.Set(respQuestionCacheKey, cacheObject, ttlDuration)
+					}
+				}
+
+				resp.Id = requestID
+				w.WriteMsg(resp)
+			}
 		}
 	}
 }
@@ -169,6 +336,17 @@ func (dnsProxy *DNSProxy) runServer(dnsServeMux *dns.ServeMux, listenAddrAndPort
 	}
 }
 
+func (dnsProxy *DNSProxy) runPeriodicTimer() {
+	ticker := time.NewTicker(time.Second * time.Duration(dnsProxy.configuration.TimerIntervalSeconds))
+	for {
+		select {
+		case <-ticker.C:
+			logger.Printf("timerPop cacheHits = %v cacheMisses = %v cache.ItemCount = %v",
+				dnsProxy.metrics.CacheHits(), dnsProxy.metrics.CacheMisses(), dnsProxy.cache.ItemCount())
+		}
+	}
+}
+
 func (dnsProxy *DNSProxy) Start() {
 	dnsServeMux := dnsProxy.createServeMux()
 
@@ -176,6 +354,8 @@ func (dnsProxy *DNSProxy) Start() {
 
 	go dnsProxy.runServer(dnsServeMux, listenAddressAndPort, "udp")
 	go dnsProxy.runServer(dnsServeMux, listenAddressAndPort, "tcp")
+
+	go dnsProxy.runPeriodicTimer()
 }
 
 func readConfiguration(configFile string) *configuration {
--- go.mod
+++ go.mod
@@ -4,11 +4,12 @@ go 1.12
 
 require (
 	github.com/kr/pretty v0.1.0
-	github.com/kr/pty v1.1.5 // indirect
-	github.com/miekg/dns v1.1.14
+	github.com/kr/pty v1.1.7 // indirect
+	github.com/miekg/dns v1.1.15
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	golang.org/x/crypto v0.0.0-20190621222207-cc06ce4a13d4 // indirect
-	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
+	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
 	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb // indirect
 	golang.org/x/text v0.3.2 // indirect
-	golang.org/x/tools v0.0.0-20190627220010-94c5763a7c84 // indirect
+	golang.org/x/tools v0.0.0-20190628222527-fb37f6ba8261 // indirect
 )
