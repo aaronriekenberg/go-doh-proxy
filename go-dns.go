532acdd7774396e5fc3855fcf3f469fcad7c360c
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-07-10 14:28:44 -0500

    Remove log.

--- go-dns.go
+++ go-dns.go
@@ -205,7 +205,6 @@ func (dnsProxy *DNSProxy) clampTTLAndCacheResponse(resp *dns.Msg) {
 
 	minTTLSeconds := dnsProxy.clampAndGetMinTTLSeconds(resp)
 	if minTTLSeconds <= 0 {
-		logger.Printf("minTTLSeconds = %v resp = \n%v", minTTLSeconds, resp.String())
 		dnsProxy.metrics.IncrementNotCachedTTL()
 		return
 	}
